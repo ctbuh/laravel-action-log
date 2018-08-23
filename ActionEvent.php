b0caa1382adb68b5cd2cbdd2503f58a07aae8a24
CTBUH\ptuskevicius <ptuskevicius@ctbuh.org>
2018-08-23 15:09:15 -0500

    added action event class

---
+++ ActionEvent.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace ctbuh\ActionLog;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ActionEvent extends Model {
+	
+	protected $guarded = array();
+	protected $fillable = array('action_name');
+	
+	public function setUpdatedAt($value){
+		// do nothing - we only want created_at
+	}
+	
+	public function user(){
+		return $this->belongsTo(config('action_log.user_model'), 'user_id');
+	}
+	
+	public function subject(){
+		return $this->morphTo('subject', 'subject_type', 'subject_id')->withTrashed();
+	}
+	
+	public function setSubject($model){
+		$this->subject()->associate($model);
+		return $this;
+	}
+	
+	// 2 args = name, value;
+	// 3 args = name, key, value
+	// 4 args = name, key, value, extra
+	public function logAction($action_name, $meta_key = null, $meta_value = null, $extra = null){
+		// TODO =--- this->save();
+		
+		if(func_num_args() == 2){
+			$meta_value = $meta_key;
+		}
+		
+		// avoid auto attribute casting to support Laravel 4
+		if(method_exists($meta_value, 'toArray')){
+			$meta_value = $meta_value->toArray();
+		}
+		
+		if(is_array($meta_value)){
+			$meta_value = json_encode($meta_value);
+		}
+		
+		$this->action_name = $action_name;
+		$this->meta_key = $meta_key;
+		$this->meta_value = $meta_value;
+		$this->extra = $extra;
+		$this->save();
+	}
+	
+	public function getMetaValue($key = null){
+		// TODO
+	}
+	
+	public function getExtra($key = null, $default = null){
+		// TODO
+	}
+	
+	public function getLabel(){
+		return sprintf('[user] performed [action] on [subject]');
+	}
+}
+
